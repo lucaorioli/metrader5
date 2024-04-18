aeb651b5728ce8c1f1e689a3979989503dce6c5a
Luca Orioli <lucaorioli@gmail.com>
2024-04-18 14:44:00 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Controlla se la cartella Metatrader non esiste e il file Metatrader.zip esiste
+if [ ! -d "./Metatrader" ] && [ -f "./Metatrader.zip" ]; then
+    # Stampa un messaggio
+    echo "La cartella Metatrader non esiste e il file Metatrader.zip è presente. Eseguendo l'estrazione..."
+
+    # Estrai il contenuto del file Metatrader.zip nella cartella corrente
+    unzip -q Metatrader.zip -d .
+fi
+
 # Ottieni il nome della directory corrente e rimuovi il percorso
 FOLDER_NAME=$(basename "$(pwd)")
 echo "La varibile numero progetto è " $FOLDER_NAME
