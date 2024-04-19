efab71c22959a5068a427e565e8707e86b336bb5
Luca Orioli <lucaorioli@gmail.com>
2024-04-19 14:58:02 +0200

    Update x11vnc-session.sh
    aggiunto -o /var/log/x11vnc.log

--- assets/x11vnc-session.sh
+++ assets/x11vnc-session.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/usr/bin/x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :0 -repeat -passwd $PW_VNC -wait 5 -permitfiletransfer -tightfilexfer -auth /var/run/slim.auth
+/usr/bin/x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :0 -repeat -passwd $PW_VNC -wait 5 -permitfiletransfer -tightfilexfer -auth /var/run/slim.auth -o /var/log/x11vnc.log
 #/usr/bin/supervisorctl restart slim
 
 
